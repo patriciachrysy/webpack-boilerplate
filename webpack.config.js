705f33e950d20e9318b5b89d057149ba22ce0060
Manezeu Patricia <pmanezeu@gmail.com>
2021-12-01 03:22:03 +0100

    Added the webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+      static: './dist',
+  },
   plugins: [
       new HtmlWebpackPlugin({
           template: './src/index.html',
@@ -11,14 +15,14 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   module: {
       rules: [
           {
               test: /\.css/i,
               use: ['style-loader', 'css-loader'],
-          }
+          },
       ],
   },
 };
